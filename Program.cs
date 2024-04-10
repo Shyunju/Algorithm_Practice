1bf65e6658cc9b86d4fbab26424b0db8689f0092
Shyunju <hyunju9908@naver.com>
2024-04-10 20:33:35 +0900

    test commint

--- Program.cs
+++ Program.cs
@@ -6,8 +6,21 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            Console.SetCursorPosition(0, 0);
+            Console.CursorVisible = false;
             Console.WriteLine("Hello World!");
-            //
+            //adsfasdfasdf
+
+            const char CIRCLE = '\u25cf';
+
+            for(int i = 0; i < 25; i++)
+            {
+                for(int j = 0; j< 25; j++)
+                {
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
