e6570eb30900582676d97b433b850062b7602d82
Shyunju <hyunju9908@naver.com>
2024-04-22 22:38:34 +0900

    플레이어 포인트 랜덤이동 구현

--- Board.cs
+++ Board.cs
@@ -8,123 +8,128 @@ namespace Algorithm
     class Board
     {
         const char CIRCLE = '\u25cf';
-        public TileType[,] _tile;
-        public int _size;
+        public TileType[,] TIle { get; private set; }
+        public int Size { get; private set; }
+
+        Player _player;
 
         public enum TileType
         {
             Empty,
             Wall,
         }
-        public void Initialize(int size)
+        public void Initialize(int size, Player player)
         {
             if (size % 2 == 0)
                 return;
-            _tile = new TileType[size, size];
-            _size = size;
+
+            _player = player;
+
+            TIle = new TileType[size, size];
+            Size = size;
 
             //GenerateByBinaryTree();
             GenerateBySideWinder();
         }
         void GenerateByBinaryTree()
         {
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
                     {
-                        _tile[y, x] = TileType.Wall;
+                        TIle[y, x] = TileType.Wall;
                     }
                     else
-                        _tile[y, x] = TileType.Empty;
+                        TIle[y, x] = TileType.Empty;
                 }
             }
 
             //랜덤으로 우 혹은 하로 길을 뚫는 작업
             Random rand = new Random();
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
                         continue;
-                    if (y == _size - 2 && x == _size - 2)
+                    if (y == Size - 2 && x == Size - 2)
                         continue;
 
-                    if (y == _size - 2)
+                    if (y == Size - 2)
                     {
-                        _tile[y, x + 1] = TileType.Empty;
+                        TIle[y, x + 1] = TileType.Empty;
                         continue;
                     }
-                    if (x == _size - 2)
+                    if (x == Size - 2)
                     {
-                        _tile[y + 1, x] = TileType.Empty;
+                        TIle[y + 1, x] = TileType.Empty;
                         continue;
                     }
 
                     if (rand.Next(0, 2) == 0)
                     {
 
-                        _tile[y, x + 1] = TileType.Empty;  //우
+                        TIle[y, x + 1] = TileType.Empty;  //우
                     }
                     else
                     {
-                        _tile[y + 1, x] = TileType.Empty;   //하
+                        TIle[y + 1, x] = TileType.Empty;   //하
                     }
                 }
             }
         }
         void GenerateBySideWinder()
         {
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
 
                     if (x % 2 == 0 || y % 2 == 0)
                     {
-                        _tile[y, x] = TileType.Wall;
+                        TIle[y, x] = TileType.Wall;
                     }
                     else
-                        _tile[y, x] = TileType.Empty;
+                        TIle[y, x] = TileType.Empty;
                 }
             }
 
             //랜덤으로 우 혹은 하로 길을 뚫는 작업
             Random rand = new Random();
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
 
                 int count = 1;
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
                         continue;
-                    if (y == _size - 2 && x == _size - 2)
+                    if (y == Size - 2 && x == Size - 2)
                         continue;
 
-                    if (y == _size - 2)
+                    if (y == Size - 2)
                     {
-                        _tile[y, x + 1] = TileType.Empty;
+                        TIle[y, x + 1] = TileType.Empty;
                         continue;
                     }
-                    if (x == _size - 2)
+                    if (x == Size - 2)
                     {
-                        _tile[y + 1, x] = TileType.Empty;
+                        TIle[y + 1, x] = TileType.Empty;
                         continue;
                     }
 
                     if (rand.Next(0, 2) == 0)
                     {
 
-                        _tile[y, x + 1] = TileType.Empty;  //우
+                        TIle[y, x + 1] = TileType.Empty;  //우
                         count++;
                     }
                     else
                     {
                         int randomIndex = rand.Next(0, count);
-                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;   //하
+                        TIle[y + 1, x - randomIndex * 2] = TileType.Empty;   //하
                         count = 1;
                     }
                 }
@@ -133,11 +138,16 @@ namespace Algorithm
         public void Render()
         {
             ConsoleColor prevColor = Console.ForegroundColor;
-            for (int y = 0; y < _size; y++)
+            for (int y = 0; y < Size; y++)
             {
-                for (int x = 0; x < _size; x++)
+                for (int x = 0; x < Size; x++)
                 {
-                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    //플레이어 좌표와 현재 좌표가 일치하면 색상 변경
+                    if (y == _player.PosY && x == _player.PosX)
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    else
+                        Console.ForegroundColor = GetTileColor(TIle[y, x]);
+
                     Console.Write(CIRCLE);
                 }
                 Console.WriteLine();
--- Program.cs
+++ Program.cs
@@ -7,14 +7,30 @@ namespace Algorithm
         static void Main(string[] args)
         {
             Board board = new Board();
-            board.Initialize(25);
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board.Size - 2, board.Size - 2, board);
 
 
             Console.SetCursorPosition(0, 0);
             Console.CursorVisible = false;
 
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
             while(true)
             {
+                #region 프레임관리
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                #endregion
+                //입력
+                //로직
+                player.Update(deltaTick);
+                //렌더링
                 Console.SetCursorPosition(0, 0);
                 board.Render();
             }
