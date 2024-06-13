e8e74e6857047ff6cca08e5554670f56105c63de
Shyunju <hyunju9908@naver.com>
2024-06-13 21:55:01 +0900

    tree Argorithm

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -3,114 +3,63 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-    class Graph
+    class TreeNode<T>
     {
-        int[,] adj = new int[6, 6]
-        {
-            {0, 15, 0, 35, 0, 0 },
-            {15, 0, 5, 10, 0, 0 },
-            {0, 5, 0, 0, 0, 0 },
-            {35, 10, 0, 0, 5, 0 },
-            {0, 0, 0, 5, 0, 5 },
-            {0, 0, 0, 0, 5, 0 }
-        };
-
-        public void Dijikstra(int start)
-        {
-            bool[] visited = new bool[6];
-            int[] distance = new int[6];
-            int[] parent = new int[6];
-
-            Array.Fill(distance, Int32.MaxValue);
-
-            distance[start] = 0;
-            parent[start] = start;
-            while(true)
-            {
-                //가장 적합한 후보의 거리와 번호를 저장
-                int closest = Int32.MaxValue;
-                int now = -1;
-                for(int i = 0; i < 6; i++)
-                {
-                    //이미 방문한 정점 스킵
-                    if (visited[i])
-                        continue;
-
-                    //아직 예약되지 않았거나, 기존 후보보다 멀리 있으면 스킵
-                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
-                        continue;
-
-                    //가장 적합한 후보 정보 갱신
-                    closest = distance[i];
-                    now = i;
-                }
-                //다음 후보가 없음 == 종료
-                if (now == -1)
-                    break;
-                
-                //제일 좋은 후보 방문
-                visited[now] = true;
-
-                //방문한 정점과 잊ㄴ접한 정점들을 조사해서
-                //상황에 따라 발견한 최단거리를 갱신
-                for(int next = 0; next < 6; next++)
-                {
-                    //연결되지 않은 정점 스킵
-                    if (adj[now, next] == 0)
-                        continue;
-
-                    //이미 방문한 정점 스킵
-                    if (visited[next])
-                        continue;
-
-                    //새로 조사된 정점의 최단거리
-                    int nextDist = distance[now] + adj[now, next];
-
-                    //기존 최단거리가 새로운 거리보다 크면 정보를 갱신
-                    if (distance[next] > nextDist)
-                    {
-                        distance[next] = nextDist;
-                        parent[next] = now;
-                    }
-                }
-            }
-        }
-        public void BFS(int start)
-        {
-            bool[] found = new bool[6];
-
-            Queue<int> q = new Queue<int>();
-            q.Enqueue(start);
-            found[start] = true;
-
-            while(q.Count> 0)
-            {
-                int now = q.Dequeue();
-                Console.WriteLine(now);
-
-                for(int next = 0; next < 6; next++)
-                {
-                    if (adj[now, next] == 0)
-                        continue;
-                    if (found[next])
-                        continue;
-                    q.Enqueue(next);
-                    found[next] = true;
-                }
-            }
-        }
-
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
     }
     class Program
     {
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                TreeNode<string> node = new TreeNode<string>() { Data = "디자인" };
+                node.Children.Add(new TreeNode<string>() { Data = "전투" }); 
+                node.Children.Add(new TreeNode<string>() { Data = "경제" }); 
+                node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                root.Children.Add(node);
+            }
+            {
+                TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍" };
+                node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                root.Children.Add(node);
+            }
+            {
+                TreeNode<string> node = new TreeNode<string>() { Data = "아트" };
+                node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                root.Children.Add(node);
+            }
+            return root;
+        }
+
+        static void PrintTree(TreeNode<string> root)
+        {
+            Console.WriteLine(root.Data);
+            foreach(TreeNode<string> child in root.Children)
+            {
+                PrintTree(child);
+            }
+        }
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+            foreach (TreeNode<string> child in root.Children)
+            {
+                int newHeigth = GetHeight(child) +1;
+                height = Math.Max(height, newHeigth);
+            }
+            return height;
+        }
         static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
-
-            Queue<int> queue = new Queue<int>();
-
-            Graph graph = new Graph();
-            graph.Dijikstra(0);
+            TreeNode<string> root = MakeTree();
+            PrintTree(root);
+            int height = GetHeight(root);
+            Console.WriteLine(height);
         }
     }
 }
